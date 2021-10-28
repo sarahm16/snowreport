484ccb4aa718a81ec7686f548332f38047a74043
Sarah Carter <carters@transblue.org>
2021-10-28 11:58:54 -0700

    removed commented out database calls from landing.js

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -13,62 +13,12 @@ function Landing() {
     //console.log(Array.isArray(zips))
 
     function addLocation() {
-        //console.log(zipcode)
-
-        // for(let i=12; i<20; i++) {
-        //     console.log(i)
-        //     let location = zipcodes.lookup(zips[i])
-        //     let locationData = {
-        //         name: `${location.state} ${location.city}`,
-        //         zipcode: location.zip,
-        //         latlong: [location.latitude, location.longitude]
-        //     }
-        //     console.log(locationData)
-
-        //     axios.post('/api/locations/add', locationData)
-        // }
-
-        // zips.forEach(zip => {
-        //     console.log(zip)
-        //     let location = zipcodes.lookup(zip)
-        //     let locationData = {
-        //         name: `${location.state} ${location.city}`,
-        //         zipcode: location.zip,
-        //         latlong: [location.latitude, location.longitude]
-        //     }
-        //     console.log(location)
-        //     //axios.post('/api/locations/add', locationData)
-        // })
-        //console.log(location)
     }
 
     async function generateReport() {
         let body = ''
 
-        axios.post('/api/locations/add', {test: 'testing'})
-
-        // axios.get('/api/locations/get')
-        //     .then(res => {
-        //         //console.log(res)
-        //         res.data.forEach(location => {
-        //             body = body.concat(`${location.name}    `).concat(`${location.zipcode     }`).concat(`<img src="https://forecast.weather.gov/meteograms/Plotter.php?lat=${location.latlong[0]}&lon=${location.latlong[1]}&wfo=OTX&zcode=WAZ036&gset=18&gdiff=8&unit=0&tinfo=PY8&ahour=0&pcmd=11011111111110000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48&pqpfhr=6&psnwhr=6" />`)
-        //             .concat('<br /><hr />')
-        //         })
-
-        //         //const encodedToken = Buffer.from(`${process.env.REACT_APP_ANVIL_KEY}:`, 'ascii').toString('base64')
-
-
-        //         const html = htmlDb
-
-        //         var test = {
-        //             data: {
-        //                 html
-        //             },
-        //           }
-
-        //         axios.post('/api/locations/generate', {x: test})
-
-        //     })
+        axios.post('/api/locations/add', {test: 'x'})
     }
 
     return(
