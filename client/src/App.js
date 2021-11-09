2a4259794de0cf31530fb1914b4dd38476f7f360
Jim Wescott <jim.wescott@transblue.org>
2021-11-09 10:17:54 -0800

    changedi nterval to 6 hours

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import html from './html';
 import html2 from './html2';
-
 import getPdf from './utils/getPdf';
 import savePdf from './utils/savePdf';
-import Landing from './pages/landing';
-import axios from 'axios';
-import getCentralTime from './utils/centralTimeZone';
 
 const schedule = require('node-schedule');
 
 function App() {
-
-  const [num, setNum] = useState(0)
-
-  // function click() {
-  //   getPdf(html).then(res => savePdf(res.data, 'A'))
-  // }
-
-  useEffect(() => {
+  useEffect(() => {     
     console.log('useeffect')
-    const getAndSavePdf = async (htmlData, version) => {
-      const pdfData = await getPdf(htmlData)
-      console.log(pdfData)
-      savePdf(pdfData, version)
-    }
-
-   //getCentralTime()
 
     // getPdf(html).then(res => savePdf(res.data, 'A'))
-    // getPdf(html2).then(res => savePdf(res.data, 'B'))
+    //   .then(() => getPdf(html2).then(res => savePdf(res.data, 'B')))
+
 
     //SCHEDULE PDF GENERATION EVERY 10 MINUTES AFTER INITIAL GENERATION
-    const job = schedule.scheduleJob('*/30 * * * *', function() {
+    const job = schedule.scheduleJob('0 */6 * * *', function() {
       console.log('Generating Pdf, last time event occured: ' + new Date())
       getPdf(html).then(res => savePdf(res.data, 'A'))
-      getPdf(html2).then(res => savePdf(res.data, 'B'))
+      .then(() => getPdf(html2).then(res => savePdf(res.data, 'B')))
     })
   }, [])
 
   return (
-    <button onClick={() => getPdf(html)}>TEST</button>
+    <button>TEST</button>
     //<Landing />
 );
 }
--- client/src/utils/savePdf.js
+++ client/src/utils/savePdf.js
@@ -21,19 +21,19 @@ function savePdf(data, version) {
     .catch(err => {
         console.log(err)
         //SEND EMAIL TO ME AND JIM IF SAVING PDF FAILS
-        // let data = JSON.stringify({  
-        //     'properties': {'__metadata': { 'type': 'SP.Utilities.EmailProperties' },  
-        //     'From': 'no-reply@transblue.org',  
-        //     'To': { 'results': ['carters@transblue.org', 'wescottj@transblue.org'] },  
-        //     'Body': err,  
-        //     'Subject': 'Snow Report Error - Saving PDF to Sharepoint' }
-        // })      
-        // axios.post('/_api/SP.Utilities.Utility.SendEmail', data, {
-        //   headers: {
-        //     "accept": "application/json;odata=verbose",
-        //     "Content-Type": "application/json;odata=verbose"
-        //   }
-        // })
+        let data = JSON.stringify({  
+            'properties': {'__metadata': { 'type': 'SP.Utilities.EmailProperties' },  
+            'From': 'no-reply@transblue.org',  
+            'To': { 'results': ['carters@transblue.org'] },  
+            'Body': '',  
+            'Subject': 'Snow Report Error - Saving PDF to Sharepoint' }
+        })      
+        axios.post('/_api/SP.Utilities.Utility.SendEmail', data, {
+          headers: {
+            "accept": "application/json;odata=verbose",
+            "Content-Type": "application/json;odata=verbose"
+          }
+        })
     })
 }
 
